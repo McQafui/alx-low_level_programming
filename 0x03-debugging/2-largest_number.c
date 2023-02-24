95d05fc7241495dd75c5bb70f8eb77d6c439b912
McQafui <McQafui@gmail.com>
2023-02-23 17:03:41 -0800

    corrected the code to display the largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -2,11 +2,9 @@
 
 /**
  * largest_number - returns the largest of 3 numbers
- *
  * @a: first integer
  * @b: second integer
  * @c: third integer
- *
  * Return: largest number
  */
 
@@ -18,17 +16,18 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
-	{
-		largest = b;
-	}
-	else if (c >b)
+	else if (a > b && c > a)
 	{
 		largest = c;
 	}
-	else
+	else if (b > c)
 	{
 		largest = b;
 	}
+	else
+	{
+		largest = c;
+	}
+
 	return (largest);
 }
