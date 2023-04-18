1b3aed799937219b2b9fe77d3e13b2e3c55b8bdb
McQafui <McQafui@gmail.com>
2023-04-18 03:24:25 -0700

    return the value of a bit at a given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,17 +7,12 @@
  *
  * Return: The value of the bit at the given index, or -1 if an error occurred.
  */
+
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= sizeof(unsigned long int) * 8) /* Check if index is out of bounds */
-		return (-1);
-
-	unsigned long int mask = 1;
-	mask <<= index;
-
-	if (n & mask)
-		return (1);
-	else
-		return (0);
+int bit;
+if (index > (sizeof(unsigned long int) * 8))
+return (-1);
+bit = ((n >> index) & 1);
+return (bit);
 }
-
